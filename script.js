a0801aaaa31f459f3caf8ea311867be3a7e5e890
Yining <ynzhang23@gmail.com>
2022-11-08 12:35:36 +0800

    Allow operator buttons to carry out its operation given inputs

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function add(inputOne, inputTwo) {
-    return inputOne + inputTwo;
+    return (+inputOne) + (+inputTwo);
 }
 
 function subtract(inputOne, inputTwo) {
-    return inputOne - inputTwo;
+    return (+inputOne) - (+inputTwo);
 }
 
 function multiply(inputOne, inputTwo) {
-    return inputOne * inputTwo;
+    return (+inputOne) * (+inputTwo);
 }
 
 function divide(inputOne, inputTwo) {
-    if (inputTwo === 0) {
+    if (inputTwo == 0) {
         return undefined;
     }
-    return inputOne / inputTwo;
+    return (+inputOne) / (+inputTwo);
 }
 
 function operate(operator, inputOne, inputTwo) {
@@ -45,15 +45,44 @@ for (i of numberButtons) {
 
 // Adding event listener to function buttons
 let functionButtons = document.querySelectorAll('.functions');
+let activeOperator;
 
 for (i of functionButtons) {
     i.addEventListener('click', function() {
-        this.classList.toggle('triggered');
+        this.classList.toggle('active');
+        activeOperator = document.querySelector('.active');
+        console.log(activeOperator);
         if (inputOne === undefined) {
             inputOne = display.textContent;
             display.textContent = '';
-        } else {
-            inputTwo = display.textContent;
-        }
+        } 
     }) 
 }
+
+console.log(functionButtons);
+
+// Registering the operator button pressed
+let equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', function() {
+    inputTwo = display.textContent;
+    switch (activeOperator.textContent) {
+        case "+":
+            display.textContent = add(inputOne, inputTwo);
+            break;
+        case "-":
+            display.textContent = subtract(inputOne, inputTwo);
+            break;
+        case "*":
+            display.textContent = multiply(inputOne, inputTwo);
+            break;
+        case "/":
+            display.textContent = divide(inputOne, inputTwo);
+            break;
+    }
+});
+
+// Upon pressing equal sign
+
+// register number on screen as inputTwo
+
+// execute selected function
